f957a42c63a80adb07ca7d23688d887962c26824
Natalie-Cardot <nataliecardot@gmail.com>
2020-12-13 21:00:43 -0800

    Add header component, restructure files

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- This card should be used as a wrapper around any content. With a slot, can feed in any HTML and will be wrapped with div with style specified in this component
+ -->
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 2rem auto;
+  max-width: 40rem;
+}
+</style>
