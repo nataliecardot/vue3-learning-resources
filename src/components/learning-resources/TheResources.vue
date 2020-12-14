9f89319bf6f3e3532f1ba201a58d52da9bf792a0
Natalie-Cardot <nataliecardot@gmail.com>
2020-12-13 21:41:42 -0800

    Use dynamic component

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-card>
+    <!-- When you add props or event listeners on custom components, by default they "fall through" to the root-level element in that custom component's template, which in this case is button element -->
+    <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <!-- Dynamic component -->
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    }
+  }
+}
+</script>
